e8e66f794c1dc88a93dfa7353a27c6a26fc84711
Jian Wu <xcb479@alumni.ku.dk>
2019-07-15 23:02:58 +0200

    Add header file

---
+++ Virtual2D/Virtual2D.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  Virtual2D
+//
+//  Created by Jian Wu on 15/07/2019.
+//  Copyright © 2019 Jian Wu. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#endif /* Header_h */
